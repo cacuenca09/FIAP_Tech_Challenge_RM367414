1ecebdd218dfb58fd4b2f1297fb0452fb74f9ee8
Camila Santos Cuenca <macos@macOSs-MacBook-Air.local>
2025-09-30 16:04:26 -0300

    ajustando para deploy

--- deploy/index.py
+++ deploy/index.py
@@ -3,6 +3,10 @@ import sys
 
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 
-from api.main import api as fastapi_api
-
-api_version = fastapi_api
+# A Vercel precisa encontrar uma variável chamada `app`
+try:
+    # Se existir módulo em api/main.py
+    from api.main import app as app  # type: ignore
+except Exception:
+    # Fallback para app/main.py
+    from app.main import app as app  # type: ignore
