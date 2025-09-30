d3d25caa82fb94f2b3fbae5c964d463dcc55d2ed
Camila Santos Cuenca <macos@macOSs-MacBook-Air.local>
2025-09-30 16:00:58 -0300

    ajustando para deploy

--- deploy/index.py
+++ deploy/index.py
@@ -1,4 +1,8 @@
-from sys import api_version
+import os
+import sys
+
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+
 from api.main import api as fastapi_api
 
 api_version = fastapi_api
